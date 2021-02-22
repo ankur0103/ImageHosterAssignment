aea157a720d390a41fdace5cc05ecff3bdf09a9d
Ankur Singh <ankur.nitk10@gmail.com>
2021-02-22 21:20:19 +0530

    New feature Added for Password Strength

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.regex.*;
 
 
 @Controller
@@ -40,10 +41,21 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, Model model) {
+
+        if(isValidPassword(user.getPassword())) {
+            userService.registerUser(user);
+            return "redirect:/users/login";
+        } else {
+            String passwordTypeError = "Password must contain at least 1 alphabet, 1 number & 1 special character";
+            model.addAttribute("User", user);
+            model.addAttribute("passwordTypeError", passwordTypeError);
+            return "users/registration";
+        }
+
     }
+
+
 
     //This controller method is called when the request pattern is of type 'users/login'
     @RequestMapping("users/login")
@@ -79,4 +91,35 @@ public class UserController {
         model.addAttribute("images", images);
         return "index";
     }
+
+
+    // Function to validate the password.
+    public boolean isValidPassword(String password) {
+
+        // Regex to check valid password.
+        String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-zA-Z])"
+                + "(?=.*[@#$%^&+=]).*$";
+
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+
+        // If the password is empty
+        // return false
+        if (password == null) {
+            return false;
+        }
+
+        // Pattern class contains matcher() method
+        // to find matching between given password
+        // and regular expression.
+        Matcher m = p.matcher(password);
+        boolean output = m.matches();
+        // Return if the password
+        // matched the ReGex
+        return output;
+    }
+
 }
+
+
